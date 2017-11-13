62f9e687cdb04b4bcd40ae105d5b29ae90aa9f35
savANDY <andicuu@gmail.com>
2017-11-13 13:53:11 +0100

    Ejercicio 7 acabado

--- JS/ARIK07ADIVINANUMERO.js
+++ JS/ARIK07ADIVINANUMERO.js
@@ -2,7 +2,41 @@ var numaleat = 0;
 var nummax = 50;
 var contador = 0;
 $(document).ready(function(){
-    
-   
+  $('#generarNum').click(function(){
+    numaleat = parseInt(Math.round(Math.random() * nummax));
+    alert("El numero aleatorio es: " + numaleat);
+
+    $('#divjuego').css("display", "block");
+  });
+
+  $('#comprobar').click(function(){
+    var numEscrito = parseInt($('#miNumero').val());
+
+    if (numEscrito == numaleat) {
+      $('#celda' + contador).css("background-color", "green");
+      $('#texto').html("<p>Has acertado, el numero es: " + numaleat + "</p>");
+      $('#divimagen').html('<img src="img/ok.png" alt="" id="imagen"/>');
+      $('#divimagen').css("display", "block");
+    } else {
+      if (contador != 4) {
+        $('#celda' + contador).css("background-color", "red");
+        contador++;
+        if (numEscrito < numaleat) {
+          mayorOmenor = "mayor";
+        } else {
+          mayorOmenor = "menor";
+        }
+        $('#texto').html("<p>El numero debe ser " + mayorOmenor + "</p>");
+
+      } else {
+        $('#celda4').css("background-color", "red");
+        $('#divimagen').css("display", "block");
+        $('#miNumero').css("display", "none");
+        $('#texto').html("<p>Has llegado al limite. GAME OVER</p>");
+      }
+      $('#miNumero').val("");
+    }
+  });
+
 
 });
