5c6495d3f2e3865713ce6a60ec79632f01481ec7
savANDY <andicuu@gmail.com>
2017-11-17 14:39:35 +0100

    Update, funciona el mostrar las peliculas de localStorage

--- JS/ARIK11zinema.js
+++ JS/ARIK11zinema.js
@@ -1,5 +1,7 @@
 sala = "ninguna";
 numeroSalas = localStorage.getItem('numPeliculas');
+var TotalButacas =[100,150,75,50];
+var ButacasLibres=[100,150,75,50];
 
 //{"titulo":"UN MONSTRUO VIENE A VERME","butacas":"150","imagen":"IMG/unmonstruo.jpg","descripcion":"A Monster Calls es una película hispano-estadounidense de fantasía dirigida por Juan Antonio Bayona, basada en la novela de 2011 del mismo nombre de Patrick Ness, quien también escribió el guion."}
 //{"titulo":"MECHANIC:RESURRECTION","butacas":"100","imagen":"img/mechanic.jpg","descripcion":"Mechanic: Resurrection es una película franco-americana de suspense y acción dirigida por Dennis Gansel. La película está protagonizada por Jason Statham, Jessica Alba, Tommy Lee Jones y Michelle Yeoh y se estrenó el 26 de agosto de 2016."}
@@ -35,76 +37,23 @@ $(document).ready(function(){
       $('#sala' + e.data.id).show();
       $('#compraentradas').show();
       $('#sala' + e.data.id).append('<h3>' + titulo + '</h3><p>' + descripcion + '</p>');
+      TotalButacas[e.data.id] = butacas;
+      ButacasLibres[e.data.id] = butacas;
+      mostrarButacas(e.data.id);
     });
   }
 
-  function esconder() {
-    $('#sala1').hide();
-    $('#sala2').hide();
-    $('#sala3').hide();
-    $('#sala4').hide();
+  function mostrarButacas(numeroSala) {
+    $("#salacompra").text("");
+    $("#salacompra").append("Total butacas: " + TotalButacas[numeroSala] + " Butacas libres: " + ButacasLibres[numeroSala]);
   }
 
-  // $('.imagen:eq(0)').click(function(){
-  //
-  //   pelicula = localStorage.getItem('peli1');
-  //   objeto = JSON.parse(pelicula);
-  //   var titulo = objeto.titulo;
-  //   var butacas = objeto.butacas;
-  //   var imagen = objeto.imagen;
-  //   var descripcion = objeto.descripcion;
-  //
-  //   esconder();
-  //       $('#sala1').html("");
-  //       $('#sala1').show();
-  //       $('#compraentradas').show();
-  //       $('#sala1').append('<h3>' + titulo + '</h3><p>' + descripcion + '</p>');
-  // });
-  //
-  // $('.imagen:eq(1)').click(function(){
-  //
-  //   pelicula = localStorage.getItem('peli2');
-  //   objeto = JSON.parse(pelicula);
-  //   var titulo = objeto.titulo;
-  //   var butacas = objeto.butacas;
-  //   var imagen = objeto.imagen;
-  //   var descripcion = objeto.descripcion;
-  //
-  //   esconder();
-  //   $('#sala2').html("");
-  //   $('#sala2').show();
-  //   $('#compraentradas').show();
-  //   $('#sala2').append('<h3>' + titulo + '</h3><p>' + descripcion + '</p>');
-  // });
-  //
-  // $('.imagen:eq(2)').click(function(){
-  //   pelicula = localStorage.getItem('peli3');
-  //   objeto = JSON.parse(pelicula);
-  //   var titulo = objeto.titulo;
-  //   var butacas = objeto.butacas;
-  //   var imagen = objeto.imagen;
-  //   var descripcion = objeto.descripcion;
-  //
-  //   esconder();
-  //   $('#sala3').html("");
-  //   $('#sala3').show();
-  //   $('#compraentradas').show();
-  //   $('#sala3').append('<h3>' + titulo + '</h3><p>' + descripcion + '</p>');
-  // });
-  //
-  // $('.imagen:eq(3)').click(function(){
-  //   pelicula = localStorage.getItem('peli4');
-  //   objeto = JSON.parse(pelicula);
-  //   var titulo = objeto.titulo;
-  //   var butacas = objeto.butacas;
-  //   var imagen = objeto.imagen;
-  //   var descripcion = objeto.descripcion;
-  //
-  //   esconder();
-  //   $('#sala4').html("");
-  //   $('#sala4').show();
-  //   $('#compraentradas').show();
-  //   $('#sala4').append('<h3>' + titulo + '</h3><p>' + descripcion + '</p>');
-  // });
+  function esconder() {
+    for (i = 1; i <= numeroSalas; i++) {
+      var sala = '#sala' + i;
+        $(sala).hide();
+    }
+
+  }
 
 });
